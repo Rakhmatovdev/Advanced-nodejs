7ac7cad23cb8e84a5dbf96074db6ee4c8c08b611
Jasur <rakhmatovjasur3@gmail.com>
2024-06-02 10:09:53 +0500

    databace & dotenv

--- project/backend/app.js
+++ project/backend/app.js
@@ -1,17 +1,30 @@
 const express = require('express');
-
+const { default: mongoose } = require('mongoose');
+const postModel = require('./models/post.model');
+require("dotenv").config()
 const app=express()
 
 app.use(express.json())
 
-app.get("/",(req,res)=>{
-    res.status(200).json({id:1,name:"Jasur",})
+app.get("/", async (req,res)=>{
+    try {      
+const allPost = await postModel.find()
+res.status(200).json(allPost)
+    } catch (error) {
+        res.status(500).json(error) 
+    }
 })
 
-app.post("/post",(req,res)=>{
-const {firstName,lastName}=req.body
-const message=`His full name - ${firstName} ${lastName}`
-    res.send(message)
+app.post("/", async (req,res)=>{
+    try {
+        const {title,body}=req.body
+const newPost=await postModel.create({title,body})
+
+    res.status(201).json(newPost)
+    } catch (error) {
+       res.status(500).json(error) 
+    }
+
 })
 
 app.delete("/:id",(req,res)=>{
@@ -27,5 +40,15 @@ app.put("/:id",(req,res)=>{
 })
 
 const PORT=process.env.PORT || 8080
+const MONGO='mongodb+srv://rakhmatovjasur3:Jasurbek3@nodejs.h3y1czg.mongodb.net/?retryWrites=true&w=majority&appName=Nodejs'
 
-app.listen(PORT,()=>console.log(`Listening on - http://localhost/${PORT}`))
+const connectMongoDB= async ()=>{
+try {
+  await mongoose.connect(MONGO).then(()=>{console.log("Connected DB...")
+  app.listen(PORT,()=>console.log(`Listening on - http://localhost/${PORT}`))
+})
+} catch (error) {
+    console.log(error);
+}
+}
+connectMongoDB()
